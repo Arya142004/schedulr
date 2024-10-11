c8eb49e71a7c859f3d90bf0d3b90db88c977a7f7
Arya142004 <aryamol21@gmail.com>
2024-10-11 15:01:59 +0530

    first commit

---
+++ src/app/(private)/events/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const EventsPage = () => {
+  return (
+    <>
+      <div>EventsPage</div>
+      <UserButton />
+    </>
+  );
+};
+
+export default EventsPage;
